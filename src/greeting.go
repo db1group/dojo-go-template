c77a096d452b7d5ae6dc4e6aa8a30cbfe6e1edfa
Ivo Batistela <irbatistela@gmail.com>
2020-08-09 20:21:51 +0000

    Apply a new hello world attempt

---
+++ src/greeting.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+}
+
+//Greet is a greeting function to welcome someone.
+func Greet(name string) string {
+	return fmt.Sprintf("Hello, %s!", name);
+}
+
--- src/greeting_test.go
+++ src/greeting_test.go
@@ -1,11 +1,11 @@
-package greeting
+package main
 
 import (
 	"testing"
 )
 
-func TestShouldSayHello(t *testing.T) {
-	greeting := SayHello("Ivo")
+func TestShouldGreet(t *testing.T) {
+	greeting := Greet("Ivo")
 
 	if "Hello, Ivo!" != greeting {
 		t.Error("Should say hello to Ivo", greeting)
